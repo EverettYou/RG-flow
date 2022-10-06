00a855ef819f1911b0f7d05c617fdfd9c2f3e94f
Everett <EverettYou@users.noreply.github.com>
2022-10-05 17:31:25 -0700

    Add images

--- RG.py
+++ RG.py
@@ -53,6 +53,64 @@ class CNN(torch.nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+
+class MarkedCNN(torch.nn.Module):
+    ''' Convolutional Neural Network (CNN) with a input mask
+        Constructs a (pooling-free) CNN with a mask to specify the
+        special sites in the input data. The data dimension can be 
+        inferred from the mask tensor dimesion.
+    
+    Example:
+        par = RGPartition([5, 5])
+        x = torch.zeros(1, 1, 5, 5)
+        net = MarkedCNN(par.mask,1)
+        net(x)
+    
+    Parameters:
+        mask :: torch.Tensor - imput mask (of the input data shape)
+        dim :: int - visible features
+        hdims :: list of int - hidden features (for all hidden layers)
+        ksize :: int - kernel size (should be odd for padding to work properly)
+        bdy_cond :: str - boundary condition 'zeros', 'reflect', 'replicate' or 'circular'
+        bias :: bool - If True, adds a learnable bias to the output
+        activation :: str - activation function name
+    '''
+    def __init__(self, mask, dim, hdims=[], ksize=3, bdy_cond='reflect', bias=True, activation='Tanh', **kwargs):
+        assert ksize is None or ksize%2==1, 'ksize={} must be an odd integer'.format(ksize)
+        super().__init__()
+        self.mask = mask
+        d = self.mask.dim()
+        try:
+            conv = {1:torch.nn.Conv1d, 2:torch.nn.Conv2d, 3:torch.nn.Conv3d}[d]
+        except:
+            raise NotImplementedError(f'CNN with data dimension {d} is not implemented.')
+        self.layers = torch.nn.ModuleList()
+        dim_in, dim_out = dim + 1, dim + 1
+        for dim_out in hdims:
+            self.layers.append(conv(dim_in, dim_out, kernel_size=ksize, 
+                    padding=(ksize-1)//2, padding_mode=bdy_cond, bias=bias))
+            dim_in = dim_out
+            self.layers.append(getattr(torch.nn, activation)())
+        self.layers.append(conv(dim_out, dim, kernel_size=ksize, 
+                padding=(ksize-1)//2, padding_mode=bdy_cond, bias=bias))
+        
+    def forward(self, x):
+        ''' CNN forward 
+            Input:
+                x :: torch.Tensor (N, dim, ...) or (dim, ...)
+                    N - batch size
+                    dim - feature dimension
+                    ... - L for 1D data 
+                        - (H,W) for 2D data
+                        - (D,H,W) for 3D data
+            Ourput:
+                cnn(x) :: torch.Tensor (same shape as input)
+        '''
+        assert x.shape[2:] == self.mask.shape, f'Data shape of the input {x.shape[2:]} does not match the mask shape {self.mask.shape}.'
+        x = torch.cat([x, self.mask[None,None,...]], axis=1) # concatenate mask as one additional feature 
+        for layer in self.layers:
+            x = layer(x)
+        return x
     
 class Dynamic(torch.nn.Module):
     ''' Converts a static base module to a time-dependent dynamic module.
@@ -268,6 +326,12 @@ class RGPartition(torch.nn.Module):
     ''' RGPartition implements bijective partition of fine-grained features
         to coarse-grained and residual features.
         
+        Example:
+        par = RGPartition([5,5])
+        x = torch.randn(1,1,5,5)
+        y, z = par.split(x)
+        x, y, z, par.merge(y, z)
+
         Parameters:
             in_shape :: torch.Size - data shape of fine-grained features
             stride :: int - stride of partition
@@ -277,19 +341,23 @@ class RGPartition(torch.nn.Module):
         super().__init__()
         self.in_shape = torch.Size(in_shape)
         out_shape = []
-        mask = torch.tensor([True])
+        self._mask = torch.tensor([True])
         for size in in_shape:
             axial_mask = torch.arange(size)%stride==stride//2
-            mask = torch.kron(mask, axial_mask)
+            self._mask = torch.kron(self._mask, axial_mask)
             out_shape.append(torch.count_nonzero(axial_mask))
         self.out_shape = torch.Size(out_shape)
-        self.register_buffer('out_indx', torch.nonzero(mask).view(-1))
-        self.register_buffer('res_indx', torch.nonzero(torch.logical_not(mask)).view(-1))
+        self.register_buffer('out_indx', torch.nonzero(self._mask).view(-1))
+        self.register_buffer('res_indx', torch.nonzero(torch.logical_not(self._mask)).view(-1))
         self.res_shape = self.res_indx.shape
             
     def extra_repr(self):
         return f'{tuple(self.in_shape)} -> {tuple(self.out_shape)}, {tuple(self.res_shape)}'
         
+    @property
+    def mask(self):
+        return self._mask.view(self.in_shape)
+    
     def split(self, x):
         ''' split x to x, z
             Input:
@@ -339,7 +407,7 @@ class RGLayer(torch.nn.Module):
     def __init__(self, in_shape, dim, **kwargs):
         super().__init__()
         self.partitioner = RGPartition(in_shape, **kwargs)
-        self.bijector = ODEBijector(Dynamic(CNN(len(in_shape), dim, **kwargs), **kwargs))
+        self.bijector = ODEBijector(Dynamic(MarkedCNN(self.partitioner.mask, dim, **kwargs), **kwargs))
             
     def encode(self, x, **kwargs):
         ''' single-layer encoding (renormalization) map x, z = R(x)
@@ -538,3 +606,4 @@ class RGModel(torch.nn.Module):
 
 
     
+
